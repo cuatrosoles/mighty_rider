526c4e251dceba71575e6090de63b9b1eba3ffc8
cuatrosoles <ubifon@gmail.com>
2023-03-21 22:30:25 -0300

    commit final

---
+++ android/app/src/main/kotlin/com/juanprogramador/navi_rider/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.juanprogramador.navi_rider
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ lib/language/LanguageEs.dart
@@ -0,0 +1,646 @@
+import 'BaseLanguage.dart';
+
+class LanguageEs extends BaseLanguage {
+  @override
+  String get appName => 'Navi Pasajero';
+
+  @override
+  String get welcomeBack => 'Bienvenido!';
+
+  @override
+  String get signInYourAccount => 'Inicie sesión en su cuenta';
+
+  @override
+  String get thisFieldRequired => 'Este campo es obligatorio';
+
+  @override
+  String get email => 'Correo electrónico';
+
+  @override
+  String get password => 'Contraseña';
+
+  @override
+  String get forgotPassword => '¿Has olvidado tu contraseña?';
+
+  @override
+  String get logIn => 'Iniciar sesión';
+
+  @override
+  String get orLogInWith => 'O iniciar sesión con';
+
+  @override
+  String get donHaveAnAccount => '¿No tienes una cuenta?';
+
+  @override
+  String get signUp => 'Registrarse';
+
+  @override
+  String get createAccount => 'Crear una cuenta';
+
+  @override
+  String get createYourAccountToContinue => 'Crea tu cuenta para continuar';
+
+  @override
+  String get firstName => 'Nombres';
+
+  @override
+  String get lastName => 'Apellidos';
+
+  @override
+  String get userName => 'Nombre de usuario';
+
+  @override
+  String get phoneNumber => 'Número de teléfono';
+
+  @override
+  String get alreadyHaveAnAccount => '¿Ya tienes una cuenta?';
+
+  @override
+  String get contactUs => 'Contáctenos';
+
+  @override
+  String get purchase => 'Compra';
+
+  @override
+  String get changePassword => 'Cambia la contraseña';
+
+  @override
+  String get oldPassword => 'Contraseña anterior';
+
+  @override
+  String get newPassword => 'Nueva contraseña';
+
+  @override
+  String get confirmPassword => 'Confirmar contraseña';
+
+  @override
+  String get passwordDoesNotMatch => 'Las contraseñas no coinciden';
+
+  @override
+  String get passwordInvalid => 'La longitud mínima de la contraseña debe ser 8';
+
+  @override
+  String get yes => 'Sí';
+
+  @override
+  String get no => 'No';
+
+  @override
+  String get writeMessage => 'Escribe un mensaje';
+
+  @override
+  String get enterTheEmailAssociatedWithYourAccount => 'Ingrese el correo electrónico asociado con su cuenta';
+
+  @override
+  String get submit => 'Enviar';
+
+  @override
+  String get language => 'Idioma';
+
+  @override
+  String get notification => 'Notificación';
+
+  @override
+  String get otpVeriFiCation => 'Verificación OTP';
+
+  @override
+  String get weHaveSentDigitCode => 'Hemos enviado un código de 4 dígitos';
+
+  @override
+  String get contactLength => 'La longitud del número de contacto debe ser de 10 a 14 dígitos.';
+
+  @override
+  String get about => 'Nosotros';
+
+  @override
+  String get useInCaseOfEmergency => 'Uso en caso de emergencia';
+
+  @override
+  String get notifyAdmin => 'Notificar a administrador';
+
+  @override
+  String get notifiedSuccessfully => 'Notificado con éxito';
+
+  @override
+  String get complain => 'Reclamo';
+
+  @override
+  String get pleaseEnterSubject => 'Por favor ingrese asunto';
+
+  @override
+  String get writeDescription => 'Escriba una descripción....';
+
+  @override
+  String get saveComplain => 'Guardar reclamo';
+
+  @override
+  String get editProfile => 'Editar perfil';
+
+  @override
+  String get gender => 'Género';
+
+  @override
+  String get address => 'Dirección';
+
+  @override
+  String get updateProfile => 'Actualización del perfil';
+
+  @override
+  String get notChangeUsername => 'No puedes cambiar el nombre de usuario';
+
+  @override
+  String get notChangeEmail => 'No puede cambiar la identificación de correo electrónico';
+
+  @override
+  String get profileUpdateMsg => 'Perfil actualizado con éxito';
+
+  @override
+  String get emergencyContact => 'Contacto de emergencia';
+
+  @override
+  String get areYouSureYouWantDeleteThisNumber => '¿Estás seguro de que quieres eliminar este número?';
+
+  @override
+  String get addContact => 'Agregar contacto';
+
+  @override
+  String get googleMap => 'Mapa de Google';
+
+  @override
+  String get save => 'Guardar';
+
+  @override
+  String get myRides => 'Mis Viajes';
+
+  @override
+  String get myWallet => 'Mi Billetera';
+
+  @override
+  String get availableBalance => 'Saldo disponible';
+
+  @override
+  String get recentTransactions => 'Transacciones Recientes';
+
+  @override
+  String get moneyDeposited => 'Dinero depositado';
+
+  @override
+  String get addMoney => 'Agregar dinero';
+
+  @override
+  String get cancel => 'Cancelar';
+
+  @override
+  String get pleaseSelectAmount => 'Seleccione la cantidad';
+
+  @override
+  String get amount => 'Monto';
+
+  @override
+  String get capacity => 'Capacidad';
+
+  @override
+  String get paymentMethod => 'Método de pago';
+
+  @override
+  String get chooseYouPaymentLate => 'Elija pagar ahora o más tarde';
+
+  @override
+  String get enterPromoCode => 'Introduce el código de promoción';
+
+  @override
+  String get confirm => 'Confirmar';
+
+  @override
+  String get wallet => 'Billetera';
+
+  @override
+  String get forInstantPayment => 'Para pago instantáneo';
+
+  @override
+  String get bookNow => 'Pedir Ahora';
+
+  @override
+  String get paymentDetail => 'Detalles de pago';
+
+  @override
+  String get rideId => 'ID de viaje';
+
+  @override
+  String get createdAt => 'Creado en';
+
+  @override
+  String get viewHistory => 'Ver historial';
+
+  @override
+  String get paymentDetails => 'Detalles del pago';
+
+  @override
+  String get paymentType => 'Tipo de pago';
+
+  @override
+  String get paymentStatus => 'Estado de pago';
+
+  @override
+  String get priceDetail => 'Detalle del precio';
+
+  @override
+  String get basePrice => 'Precio base';
+
+  @override
+  String get distancePrice => 'Precio de distancia';
+
+  @override
+  String get timePrice => 'Precio de tiempo';
+
+  @override
+  String get waitTime => 'Tiempo de espera';
+
+  @override
+  String get extraCharges => 'Cargos extras';
+
+  @override
+  String get couponDiscount => 'Descuento de cupón';
+
+  @override
+  String get total => 'Total';
+
+  @override
+  String get payment => 'Pago';
+
+  @override
+  String get cash => 'Efectivo';
+
+  @override
+  String get updatePaymentStatus => 'Actualizar el estado de pago';
+
+  @override
+  String get waitingForDriverConformation => 'Esperando la confirmación del conductor';
+
+  @override
+  String get continueNewRide => 'Continuar con un nuevo viaje';
+
+  @override
+  String get payToPayment => 'Pagar al pago';
+
+  @override
+  String get tip => 'Propina';
+
+  @override
+  String get pay => 'Pagar';
+
+  @override
+  String get howWasYourRide => '¿Cómo estuvo tu viaje?';
+
+  @override
+  String get wouldYouLikeToAddTip => '¿Le gustaría agregar la propina?';
+
+  @override
+  String get addMoreTip => 'Agregar más propina';
+
+  @override
+  String get addMore => 'Añadir más';
+
+  @override
+  String get addReviews => 'Agregar reseñas';
+
+  @override
+  String get writeYourComments => 'Escribe tus comentarios...';
+
+  @override
+  String get continueD => 'Continuar';
+
+  @override
+  String get detailScreen => 'Pantalla de detalle';
+
+  @override
+  String get aboutDriver => 'Acerca del conductor';
+
+  @override
+  String get rideHistory => 'Historial de viaje';
+
+  @override
+  String get myProfile => 'Mi Perfil';
+
+  @override
+  String get myTrips => 'Mis Viajes';
+
+  @override
+  String get emergencyContacts => 'Contactos de Emergencia';
+
+  @override
+  String get logOut => 'Cerrar sesión';
+
+  @override
+  String get areYouSureYouWantToLogoutThisApp => '¿Estás seguro de que quieres cerrar sesión?';
+
+  @override
+  String get whatWouldYouLikeToGo => '¿A dónde te gustaría ir?';
+
+  @override
+  String get enterYourDestination => 'Ingrese su destino';
+
+  @override
+  String get currentLocation => 'Ubicación actual';
+
+  @override
+  String get destinationLocation => 'Ubicación de destino';
+
+  @override
+  String get chooseOnMap => 'Elija en el mapa';
+
+  @override
+  String get profile => 'Perfil';
+
+  @override
+  String get theme => 'Tema';
+
+  @override
+  String get privacyPolicy => 'Política de Privacidad';
+
+  @override
+  String get helpSupport => 'Servicio de Asistencia';
+
+  @override
+  String get termsConditions => 'Términos y Condiciones';
+
+  @override
+  String get aboutUs => 'Sobre Nosotros';
+
+  @override
+  String get lookingForNearbyDrivers => 'Buscando conductores cercanos';
+
+  @override
+  String get weAreLookingForNearDriversAcceptsYourRide => 'Estamos buscando conductores cercanos\n que acepten su viaje';
+
+  @override
+  String get areYouSureYouWantToCancelThisRide => '¿Estás seguro de que quieres cancelar este viaje?';
+
+  @override
+  String get serviceDetail => 'Detalle de servicio';
+
+  @override
+  String get get => 'Obtener';
+
+  @override
+  String get rides => 'Viajes';
+
+  @override
+  String get people => 'Personas';
+
+  @override
+  String get fare => 'Tarifa';
+
+  @override
+  String get done => 'Hecho';
+
+  @override
+  String get availableOffers => 'Ofertas disponibles';
+
+  @override
+  String get off => 'APAGADO';
+
+  @override
+  String get sendOTP => 'Enviar OTP';
+
+  @override
+  String get otpVerification => 'Verificación OTP';
+
+  @override
+  String get enterTheCodeSendTo => 'Ingrese el código enviado a';
+
+  @override
+  String get didNotReceiveTheCode => 'No recibió el código';
+
+  @override
+  String get resend => 'Reenviar';
+
+  @override
+  String get carModel => 'Modelo de auto';
+
+  @override
+  String get sos => 'Llamada SOS';
+
+  @override
+  String get driverReview => 'Revisión del conductor';
+
+  @override
+  String get signInUsingYourMobileNumber => 'Inicie sesión usando su número celular';
+
+  @override
+  String get otp => 'OTP';
+
+  @override
+  String get newRideRequested => 'Nuevo viaje solicitado';
+
+  @override
+  String get accepted => 'Aceptado';
+
+  @override
+  String get arriving => 'Yendo';
+
+  @override
+  String get arrived => 'Llegado';
+
+  @override
+  String get inProgress => 'En progreso';
+
+  @override
+  String get cancelled => 'Cancelado';
+
+  @override
+  String get completed => 'Terminado';
+
+  @override
+  String get pleaseEnableLocationPermission => 'Habilite el permiso de ubicación';
+
+  @override
+  String get pending => 'Pendiente';
+
+  @override
+  String get failed => 'Ha fallado';
+
+  @override
+  String get paid => 'Pagado';
+
+  @override
+  String get male => 'Masculino';
+
+  @override
+  String get female => 'Femenino';
+
+  @override
+  String get other => 'Otro';
+
+  @override
+  String get deleteAccount => 'Borrar cuenta';
+
+  @override
+  String get account => 'Cuenta';
+
+  @override
+  String get areYouSureYouWantPleaseReadAffect => '¿Estás seguro de que quieres eliminar tu cuenta? Lea cómo afectará la eliminación de la cuenta.';
+
+  @override
+  String get deletingAccountEmail =>
+      'Eliminar su cuenta elimina la información personal de nuestra base de datos. Su correo electrónico se reserva permanentemente y el mismo correo electrónico no se puede reutilizar para registrar una nueva cuenta';
+
+  @override
+  String get areYouSureYouWantDeleteAccount => '¿Estás seguro de que quieres eliminar la cuenta?';
+
+  @override
+  String get yourInternetIsNotWorking => 'Tu Internet no funciona';
+
+  @override
+  String get allow => 'Permitir';
+
+  @override
+  String get mostReliableMightyRiderApp => 'Aplicación de reservas de taxis más confiable';
+
+  @override
+  String get toEnjoyYourRideExperiencePleaseAllowPermissions => 'Para disfrutar de su experiencia de viaje\n por favor permítanos algunos permisos';
+
+  @override
+  String get txtURLEmpty => 'URL está vacía';
+
+  @override
+  String get lblFollowUs => 'Síganos';
+
+  @override
+  String get setting => 'Ajuste';
+
+  @override
+  String get duration => 'Duración';
+
+  @override
+  String get paymentVia => 'Pago a través de';
+
+  @override
+  String get demoMsg => 'El rol de probador no se permite realizar esta acción';
+
+  @override
+  String get findPlace => 'Encontrar un lugar...';
+
+  @override
+  String get pleaseWait => 'Espere por favor';
+
+  @override
+  String get selectPlace => 'Seleccionar lugar';
+
+  @override
+  String get placeNotInArea => 'Colocar no en el área';
+
+  @override
+  String get youCannotChangePhoneNumber => 'No puedes cambiar el número de teléfono';
+
+  @override
+  String get complainList => 'Lista de Reclamos';
+
+  @override
+  String get writeMsg => 'Escribe un mensaje';
+
+  @override
+  String get pleaseEnterMsg => 'Por favor ingrese el mensaje';
+
+  @override
+  String get viewAll => 'Ver todo';
+
+  @override
+  String get pleaseSelectRating => 'Seleccione la calificación';
+
+  @override
+  String get moneyDebit => 'Débito de dinero';
+
+  @override
+  String get pleaseAcceptTermsOfServicePrivacyPolicy => 'Acepte los términos de la política de servicio y privacidad';
+
+  @override
+  String get rememberMe => 'Recuérdame';
+
+  @override
+  String get iAgreeToThe => 'Estoy de acuerdo con la';
+
+  @override
+  String get driverInformation => 'Información del conductor';
+
+  @override
+  String get service => 'Servicio';
+
+  @override
+  String get nameFieldIsRequired => 'Se requiere un campo de nombre';
+
+  @override
+  String get phoneNumberIsRequired => 'Se requiere número de teléfono';
+
+  @override
+  String get enterName => 'Ingrese su nombre';
+
+  @override
+  String get enterContactNumber => 'Ingrese el número de contacto';
+
+  @override
+  String get bookingRideForOthers => 'Reservar un viaje para otros';
+
+  @override
+  String get selectGender => 'Seleccione género';
+
+  @override
+  String get invoice => 'Factura';
+
+  @override
+  String get customerName => 'Nombre del cliente';
+
+  @override
+  String get sourceLocation => 'Ubicación de origen';
+
+  @override
+  String get invoiceNo => 'Factura N°';
+
+  @override
+  String get invoiceDate => 'Fecha de la factura';
+
+  @override
+  String get orderedDate => 'Fecha ordenada';
+
+  @override
+  String get lblCarNumberPlate => 'Matrícula de coche';
+
+  @override
+  String get lblCouponCode => 'Código promocional:';
+
+  @override
+  String get lblRide => 'Viaje';
+
+  @override
+  String get lblRideInformation => 'Información de viaje';
+
+  @override
+  String get lblWhereAreYou => '¿Dónde estás?';
+
+  @override
+  String get lblDropOff => 'Dejar en';
+
+  @override
+  String get lblDistance => 'Distancia:';
+
+  @override
+  String get lblWhoRiding => '¿Quién viajará?';
+
+  @override
+  String get lblSomeoneElse => 'Alguien más';
+
+  @override
+  String get lblYou => 'Tú';
+
+  @override
+  String get lblWhoRidingMsg => 'Confirme al pasajero y la información del viaje';
+
+  @override
+  String get lblNext => 'Siguiente';
+
+  @override
+  String get lblLessWalletAmount => 'Nota: tiene un saldo insuficiente en su billetera. Agregue saldo de lo contrario, debe pagar en efectivo.';
+
+  @override
+  String get lblPayWhenEnds => 'Pagar cuando termine el viaje';
+}
